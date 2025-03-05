de746cc7e0ad4ad85a3518efd50342529de75fce
Connor J Thompson <ct770a@gmail.com>
2025-03-05 23:51:11 +1100

    added draft comments

--- NetHookInject.cpp
+++ NetHookInject.cpp
@@ -6,15 +6,20 @@
 int main()
 {
     std::cout << "Hello World!\n";
+
+
+    // look for all halo processes
+
+    // preload dll so we can get the addresses for stuff
+    // specifically the event log thing
+
+    // check to see if dll is already injected
+    // if it is, then skip straight to the looping
+
+    // hook in dll to the target process
+    // let the dll do its stuff
+
+    // then just run a loop within this tool to read the injected dlls event log\
+
+    // readmem on the eventlog address to get the ptr and whatever
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
